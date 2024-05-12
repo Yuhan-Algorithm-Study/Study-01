bd7ca815aaadf253b44ef357cd3edae9421e1127
Chang Yeol <ckdduf138@naver.com>
2024-05-12 21:00:30 +0900

    Update lcy_31671.cpp

--- acmicpc/31671/lcy_31671.cpp
+++ acmicpc/31671/lcy_31671.cpp
@@ -4,37 +4,51 @@
 
 using namespace std;
 
-int main()
+int n, m;
+pair<int, int> directions[2][2] = { { {-1, -1}, {1, -1} }, { {-1, 1}, {1, 1} } };
+
+int bfs(vector<vector<int>>& map)
 {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-	int n, m;	cin >> n >> m;
-
-	vector<vector<int>> map(n + 1, vector<int>(2 * n + 1, 0));
-
-	for (int i = 0; i < m; i++)
-	{
-		int x, y;	cin >> x >> y;
-		map[n - y][x] = 2;
-	}
-
 	queue<pair<int, int>> q;
-	vector<pair<int, int>> direct = { { -1, 1}, {1 , 1} };
 
-	q.push({ n, 0 });
-	map[n][0] = 1;
-
-	int ans = -1;
+	// step1
+	q.push({ n, 2 * n });
+	map[n][2 * n] = 0;
 
 	while (!q.empty())
 	{
 		pair<int, int> curr = q.front();
 		q.pop();
 
-		for (int dir = 0; dir < 2; dir++)
+		for (const auto& dir : directions[0])
 		{
-			int ny = curr.first + direct[dir].first;
-			int nx = curr.second + direct[dir].second;
+			int ny = curr.first + dir.first;
+			int nx = curr.second + dir.second;
+
+			if (ny < 0 || nx < 0 || ny >= n + 1 || nx >= 2 * n + 1) continue;
+			if (map[ny][nx] == 2 || map[ny][nx] == 0)				continue;
+			if (n - ny > nx)										continue;
+
+			q.push({ ny, nx });
+			map[ny][nx] = 0;
+		}
+	}
+
+	// step2
+	int ans = -1;
+
+	q.push({ n, 0 });
+	map[n][0] = 1;
+
+	while (!q.empty())
+	{
+		pair<int, int> curr = q.front();
+		q.pop();
+
+		for (const auto& dir : directions[1])
+		{
+			int ny = curr.first + dir.first;
+			int nx = curr.second + dir.second;
 
 			if (ny < 0 || nx < 0 || ny >= n + 1 || nx >= 2 * n + 1) continue;
 			if (map[ny][nx] > 0)									continue;
@@ -47,7 +61,24 @@ int main()
 		}
 	}
 
-	map[n][2 * n] != 1 ? cout << -1 : cout << ans;
+	return map[n][2 * n] != 1 ? -1 : ans;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+
+	vector<vector<int>> map(n + 1, vector<int>(2 * n + 1, 3));
+
+	for (int i = 0; i < m; i++)
+	{
+		int x, y;	cin >> x >> y;
+		map[n - y][x] = 2;
+	}
+
+	cout << bfs(map);
 
 }
 
