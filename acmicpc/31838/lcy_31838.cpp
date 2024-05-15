2332d14609508d9f1c140d29711b7d9e681cd04a
Chang Yeol <ckdduf138@naver.com>
2024-05-15 21:06:06 +0900

    Update lcy_31838.cpp

---
+++ acmicpc/31838/lcy_31838.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int k, n;
+
+long long dp(vector<long long>& vec)
+{
+	vector<long long> dp(k + 1, 0);
+	vector<long long> sum(k + 1, 0);
+
+	long long res = vec[0];
+
+	for (int i = 1; i < k + 1; i++)
+	{
+		sum[i] += sum[i - 1] + vec[i];
+	}
+
+	long long ans = -500001;
+	for (int idx = 1; idx < k + 1; idx++)
+	{
+		if (sum[idx] > res + sum[idx + n <= k ? idx + n : k] - sum[idx - 1])
+		{
+			dp[idx] = sum[idx];
+			res = max(res, dp[idx]);
+		}
+		else
+		{
+			dp[idx] = res + sum[idx + n <= k ? idx + n : k] - sum[idx - 1];
+
+			res = 0;
+		}
+
+		ans = max(dp[idx], ans);
+		cout << dp[idx] << " ";
+	}
+
+	return ans;
+}
+
+int main() 
+{
+	cin >> k >> n;
+
+	vector<long long> vec(k + 1, 0);
+
+	for (int i = 1; i < k + 1; i++)
+	{
+		cin >> vec[i];
+	}
+
+	cout << dp(vec);
+
+}
+
+/*
+6 5
+1 -10 -10 -10 1 1
+*/
